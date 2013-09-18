20fd8425f53a6bae6e56f98217918eea56d4a5d9
walkerning <foxdoraame@gmail.com>
2013-09-18 19:01:02 +0800

    first commit

---
+++ DoudouView.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from Doudouitem import *
+
+
+
+
+class DouDouView(QGraphicsView):
+    def __init__(self, parent = None):
+        super(DouDouView, self).__init__(parent)
+
+        
---
+++ Doudouitem.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+#豆豆item
+
+from PyQt4.QtGui import *
+form PyQt4.QtCore import *
+import qrc_resource
+
+UNIT_WIDTH = 20
+EDGE_WIDTH = 1
+class DouBack(QGraphicsObject):
+    def __init__(self, parent = None):
+        super(DouBack, self).__init__(parent)
+
+    def boundingRect(self):
+        return QRect(0 ,0, UNIT_WIDTH + EDGE_WIDTH, UNIT_WIDTH + EDGE_WIDTH)
+
+class DouDou(DouBack):
+    def __init__(self, color, parent = None):
+        super(Doudou, self).__init__(parent)
+        self.color = QColor(*color)
+
+    def paint(self, painter, option, widget = None):
+        #暂时用圆代替
+        painter.save()
+        brush = QBrush(Qt.SolidPattern)
+        brush.setColor(self.color)
+        painter.setBrush(brush)
+        painter.drawEllipse(EDGE_WIDTH/2, EDGE_WIDTH / 2, UNIT_WIDTH, UNIT_WIDTH)
+        painter.restore()
+
+    def __eq__(self, obj):
+        return (self.color == obj.color)
+
+class Balk(DouBack):
+    def __init__(self, color, parent = None):
+        super(Doudou, self).__init__(parent)
+        self.picture = QImage(":balk.png").scaled(UNIT_WIDTH, UNIT_WIDTH)
+
+    def paint(self, painter, option, widget = None):
+        #找个图片来
+        painter.save()
+        painter.setCompositionMode(QPainter.Multiply)
+        painter.drawImage(QPointF(EDGE_WIDTH/2, EDGE_WIDTH/2), self.picture)
