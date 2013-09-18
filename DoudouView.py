67de0976822d80821dfbd48d5fc3ff3e784ec2e0
piggyfeilian <fjmpiggy@gmail.com>
2013-09-18 22:13:41 +0800

    这次教猪怎么用git

--- DoudouView.py
+++ DoudouView.py
@@ -7,7 +7,17 @@ from Doudouitem import *
 
 
 class DouDouView(QGraphicsView):
-    def __init__(self, parent = None):
+    def __init__(self, scene, parent = None):
         super(DouDouView, self).__init__(parent)
-
         
+        self.scene = scene
+        self.setScene(self.scene)
+        #默认size
+        self.size = (10, 10)
+        self.douMap = []
+    def setDoudou(self, douMap):
+        for i in range(len(Dou)):
+            self.douMap.append([])
+            for j in range(len(Dou[i])):
+                self.douMap[i].append(Dou[i][j])
+                new_douMap = DouBack()#教猪
--- Doudouitem.py
+++ Doudouitem.py
@@ -7,16 +7,27 @@ import qrc_resource
 
 UNIT_WIDTH = 20
 EDGE_WIDTH = 1
+
+def GetPos(x, y):
+    return QPointF(x * (UNIT_WIDTH + EDGE_WIDTH), y * (UNIT_WIDTH + EDGE_WIDTH))
 class DouBack(QGraphicsObject):
-    def __init__(self, parent = None):
+    def __init__(self, x = 0, y = 0,parent = None):
         super(DouBack, self).__init__(parent)
+        self.corX = x
+        self.corY = y
+        self.setPos(GetPos(x,y))
+        
+    def setPos(self, x, y):
+        self.corX = x
+        self.corY = y
+        self.setPos(GetPos(x,y))
 
     def boundingRect(self):
         return QRect(0 ,0, UNIT_WIDTH + EDGE_WIDTH, UNIT_WIDTH + EDGE_WIDTH)
 
 class DouDou(DouBack):
-    def __init__(self, color, parent = None):
-        super(Doudou, self).__init__(parent)
+    def __init__(self, x, y, color, parent = None):
+        super(Doudou, self).__init__(x, y, parent)
         self.color = QColor(*color)
 
     def paint(self, painter, option, widget = None):
@@ -32,8 +43,8 @@ class DouDou(DouBack):
         return (self.color == obj.color)
 
 class Balk(DouBack):
-    def __init__(self, color, parent = None):
-        super(Doudou, self).__init__(parent)
+    def __init__(self, x, y, parent = None):
+        super(Doudou, self).__init__(x, y, parent)
         self.picture = QImage(":balk.png").scaled(UNIT_WIDTH, UNIT_WIDTH)
 
     def paint(self, painter, option, widget = None):
